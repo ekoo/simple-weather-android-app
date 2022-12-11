7c303d0e5dea1e2a58bdf949a1043d015abe4565
Eko <eko.yulianto1997@gmail.com>
2022-12-11 07:23:18 +0700

    Update Dependencies

--- app/src/main/java/com/ekoo/weather/MainActivity.kt
+++ app/src/main/java/com/ekoo/weather/MainActivity.kt
@@ -1,47 +1,49 @@
 package com.ekoo.weather
 
-import android.annotation.SuppressLint
 import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.ekoo.weather.databinding.ActivityMainBinding
 import com.robin.locationgetter.EasyLocation
-import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var viewModel: MainViewModel
-    @SuppressLint("SimpleDateFormat", "SetTextI18n")
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.apply {
-            locationName.observe(this@MainActivity, Observer {
-                location_tv.text = it
-            })
-            oneHourForecast.observe(this@MainActivity, Observer {
-                name_tv.text = it[0].iconPhase
-                current_temperatur_tv.text = "${it[0].temperature.value.toInt()}°F"
+            locationName.observe(this@MainActivity) {
+                binding.locationTv.text = it
+            }
+            oneHourForecast.observe(this@MainActivity) {
+                binding.nameTv.text = it[0].iconPhase
+                binding.currentTemperaturTv.text = "${it[0].temperature.value.toInt()}°F"
 
                 val rawDate = it[0].dateTime.take(10)
                 val oldFormat = SimpleDateFormat("yyyy-MM-dd")
-                val date= oldFormat.parse(rawDate)
+                val date = oldFormat.parse(rawDate)
                 val newFormat = SimpleDateFormat("dd-MMM-yyyy")
 
-                if (date != null){
-                    date_tv.text = newFormat.format(date)
+                if (date != null) {
+                    binding.dateTv.text = newFormat.format(date)
                 }
 
-            })
-            oneDayForecast.observe(this@MainActivity, Observer {
+            }
+            oneDayForecast.observe(this@MainActivity) {
 
-                low_temperatur_tv.text = it.dailyForecasts[0].temperature.minimum.value.toString()
-                high_temperatur_tv.text = it.dailyForecasts[0].temperature.maximum.value.toString()
+                binding.lowTemperaturTv.text =
+                    it.dailyForecasts[0].temperature.minimum.value.toString()
+                binding.highTemperaturTv.text =
+                    it.dailyForecasts[0].temperature.maximum.value.toString()
 
-            })
+            }
         }
 
         EasyLocation(this, object : EasyLocation.EasyLocationCallBack{
